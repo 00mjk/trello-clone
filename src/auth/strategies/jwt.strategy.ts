f7af88a3e04a36a3ef8ee3d1175fa03395ea9072
kitt3911 <kitt3911@users.noreply.github.com>
2021-03-30 07:35:46 +0000

    Prettified Code!

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,7 +7,10 @@ import { UsersService } from '../../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService,private readonly userService: UsersService) {
+  constructor(
+    configService: ConfigService,
+    private readonly userService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: JWT_SECRET,
@@ -16,9 +19,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    const user = await this.userService.findByEmail(payload.email)  
-    if(!user){
-      throw new UnauthorizedException()
+    const user = await this.userService.findByEmail(payload.email);
+    if (!user) {
+      throw new UnauthorizedException();
     }
     return user;
   }

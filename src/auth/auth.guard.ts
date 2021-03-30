f749c242caab99dfc1ea6c59355035fcf6cdce0c
kitt3911 <kitt3911@users.noreply.github.com>
2021-03-30 05:32:58 +0000

    Prettified Code!

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -9,15 +9,17 @@ import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(@Inject('UsersService') private readonly  userService:UsersService){
+  constructor(
+    @Inject('UsersService') private readonly userService: UsersService,
+  ) {
     super();
   }
-  canActivate(context: ExecutionContext){
+  canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    const { email } = request.body
+    const { email } = request.body;
     //const user = await this.userService.findByEmail(email)
     //request.user = user;
-    return super.canActivate(context)
+    return super.canActivate(context);
   }
 
   handleRequest(err, user, info) {
